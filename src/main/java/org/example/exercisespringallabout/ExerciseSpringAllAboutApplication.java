21cdc4fdd1f8c1605d7827940590f2f2c5124004
srang03 <srang_@naver.com>
2025-03-31 10:38:34 +0900

    [init] 프로젝트 구조 세팅

---
+++ src/main/java/org/example/exercisespringallabout/ExerciseSpringAllAboutApplication.java
@@ -0,0 +1,13 @@
+package org.example.exercisespringallabout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseSpringAllAboutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExerciseSpringAllAboutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/exercisespringallabout/ExerciseSpringAllAboutApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.exercisespringallabout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseSpringAllAboutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
